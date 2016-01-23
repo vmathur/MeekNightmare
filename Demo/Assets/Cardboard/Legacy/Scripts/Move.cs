ab88ad2356f70ed9322c6c68958a494d0b7d81da
Vishal Mathur <vishmathur5@gmail.com>
2016-01-23 05:03:38 -0500

    collision kind of working

---
+++ Demo/Assets/Cardboard/Legacy/Scripts/Move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	private CardboardHead head;
+	public float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+		head = Camera.main.GetComponent<StereoController>().Head;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += speed * head.Gaze.direction;
+
+	}
+}
