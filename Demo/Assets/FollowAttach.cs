f71e8124e95db03b6cd0d5215668da2ddcadb140
Vishal Mathur <vishmathur5@gmail.com>
2016-01-24 03:45:25 -0500

    drake follows you around walls now

---
+++ Demo/Assets/FollowAttach.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowAttach : MonoBehaviour {
+	NavMeshAgent agent;
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination (target.position);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		print (collision.gameObject.name);
+	}
+}
