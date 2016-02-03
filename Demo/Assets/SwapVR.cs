9a2613d61bb139fb25071f889c20d4cf2b07bb3f
Vishal Mathur <vishmathur5@gmail.com>
2016-02-02 20:27:58 -0500

    added another drake, better maze

---
+++ Demo/Assets/SwapVR.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwapVR : MonoBehaviour {
+
+	public GameObject[] cardboardObjects;
+	public GameObject[] monoObjects;
+
+	// Turn on or off VR mode
+	void ActivateVRMode(bool goToVR) {
+		foreach (GameObject cardboardThing in cardboardObjects) {
+			cardboardThing.SetActive(goToVR);
+		}
+		foreach (GameObject monoThing in monoObjects) {
+			monoThing.SetActive(!goToVR);
+		}
+		Cardboard.SDK.VRModeEnabled = goToVR;
+
+		// Tell the game over screen to redisplay itself if necessary
+//		gameObject.GetComponent<GameController>().RefreshGameOver();
+	}
+
+	public void Switch() {
+		ActivateVRMode(!Cardboard.SDK.VRModeEnabled);
+	}
+
+	void Update () {
+		if (Cardboard.SDK.BackButtonPressed) {
+			Switch();
+		}
+	}
+
+	void Start() {
+		ActivateVRMode(false);
+	}
+}
